18f62015158677279910d9121acb90ea30737044
sanjeevkomma <sanjeevkomma@gmail.com>
2022-02-13 19:16:02 +0530

    in

---
+++ java-8/src/main/java/com/demo/collectors_class/Collectors_GroupingBy.java
@@ -0,0 +1,27 @@
+package com.demo.collectors_class;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Collectors_GroupingBy {
+
+   public static void main(String[] args) {
+
+      List<String> names =
+          Arrays.asList("Jon", "Ajeet", "Steve",
+             "Ajeet", "Jon", "Ajeet");
+
+      Map<String, Long> map =
+      names.stream().collect(
+          Collectors.groupingBy(
+             Function.identity(), Collectors.counting()
+          )
+      );
+
+      System.out.println(map);
+
+   }
+}
