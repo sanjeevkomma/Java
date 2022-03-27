f65a560a15af4e82db7fc9305476d5a694b870f7
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-03-27 23:21:11 +0530

    in

---
+++ multi-threading/src/main/java/ExecutorServiceExample.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {  
+
+	public static void main(String[] args) {  
+
+		ExecutorService executorService = Executors.newSingleThreadExecutor();  
+
+		executorService.execute(new Runnable() {  
+
+			public void run() {  
+
+				System.out.println("ExecutorService");  
+
+			}  
+
+		});  
+
+		executorService.shutdown();  
+	}  
+}   
