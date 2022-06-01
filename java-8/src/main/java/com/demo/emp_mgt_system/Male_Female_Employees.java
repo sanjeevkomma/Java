a4da4cbefa2b98665400c40ef88e32e27b2afd6c
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-06-01 17:55:11 +0530

    in

--- java-8/src/main/java/com/demo/emp_mgt_system/Male_Female_Employees.java
+++ java-8/src/main/java/com/demo/emp_mgt_system/Male_Female_Employees.java
@@ -2,19 +2,22 @@ package com.demo.emp_mgt_system;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import com.demo.algos.model.Employee;
 
 public class Male_Female_Employees {
-
+	
 	public static void main(String[] args) {
 		
 		List<Employee> employeeList = EmployeeList.getEmployeeList();
 
 		Map<String, Long> noOfMaleAndFemaleEmployees =
 				
-				employeeList.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+				employeeList.stream().collect(Collectors.groupingBy( Employee :: getGender, Collectors.counting() ));
 				         
 				System.out.println(noOfMaleAndFemaleEmployees);
 		
